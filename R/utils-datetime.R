b082f6bb383377c51417e55874a0e624bdbb38d5
Roberto Villegas-Diaz <bthillo@hotmail.com>
2022-11-07 17:04:28 +0000

    Add support for minutes

--- R/utils-datetime.R
+++ R/utils-datetime.R
@@ -12,13 +12,15 @@
 #' @export
 #'
 #' @examples
-#' duration2datetime("a day ago")
+#' duration2datetime("a minute ago")
 #' duration2datetime("an hour ago")
+#' duration2datetime("a day ago")
 #' duration2datetime("a week ago")
 #' duration2datetime("a month ago")
 #' duration2datetime("a year ago")
-#' duration2datetime("2 days ago")
+#' duration2datetime("2 minutes ago")
 #' duration2datetime("2 hours ago")
+#' duration2datetime("2 days ago")
 #' duration2datetime("2 weeks ago")
 #' duration2datetime("2 months ago")
 #' duration2datetime("2 years ago")
@@ -44,9 +46,10 @@ duration2datetime <- function(str,
       units <- str %>%
         stringr::str_remove_all("^an|^a") %>%
         stringr::str_remove("ago$") %>%
-        stringr::str_squish()
+        stringr::str_squish() %>%
+        stringr::str_replace("minute", "min")
       time_diff <- as.difftime(1, units = stringr::str_c(units, "s"))
-    } else if (stringr::str_detect(str, "hour|day|week")) {
+    } else if (stringr::str_detect(str, "minute|hour|day|week")) {
       units <- str %>%
         stringr::str_remove_all("ago") %>%
         stringr::str_squish() %>%
@@ -56,7 +59,9 @@ duration2datetime <- function(str,
         stringr::str_remove(units) %>%
         stringr::str_squish() %>%
         as.integer()
-      time_diff <- as.difftime(value, units = units)
+      time_diff <- as.difftime(value,
+                               units = units %>%
+                                 stringr::str_replace("minute", "min"))
     } else {
       return(NA)
     }
