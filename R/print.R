8b3a8add467b59767ae52fc64da870f9db7f1102
Roberto Villegas-Diaz <bthillo@hotmail.com>
2022-12-15 10:07:41 +0000

    Update the print.gmaps_reviews to display a different message if a place does not have reviews

--- R/print.R
+++ R/print.R
@@ -4,17 +4,29 @@
 #' @param ... further arguments passed to or from other methods.
 #' @rdname print
 #' @export
-print <- function(x, ...) {
-  print(x, ...)
-}
+#'
+#' @name print
+NULL
 
+#' Print Google Maps' reviews
 #' @rdname print
 #' @export
 print.gmaps_reviews <- function(x, ...) {
-  message("This place has an overall rating of ",
-          attr(x, "stars"),
-          " stars and ",
-          attr(x, "total_reviews"),
-          " total reviews.")
+  name <- attr(x, "name")
+  place_id <- attr(x, "place_id")
+  stars <- attr(x, "stars")
+  total_reviews <- attr(x, "total_reviews")
+
+  if (is.na(total_reviews) || total_reviews == 0) {
+    message("This place (", name, ") has no reviews.")
+  } else {
+    message("This place (",
+            name,
+            ") has an overall rating of ",
+            stars,
+            " stars and ",
+            total_reviews,
+            " total reviews.")
+  }
   NextMethod()
 }
